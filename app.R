d1080e110dcab7653f5f13f4cbafc6969ac274db
TerminusBot <terminus2049@protonmail.com>
2018-07-17 01:20:09 +0800

    新增被删类型筛选

--- app.R
+++ app.R
@@ -3,16 +3,29 @@ library(shiny)
 library(readr)
 
 createLink <- function(link, val) {
-  l = paste0('<a href="http://wechatscope.jmsc.hku.hk:8000/html?fn=', link, '" target="_blank" class="btn btn-primary">查看全文</a>')
+  l = paste0('<a href="http://wechatscope.jmsc.hku.hk:8000/html?fn=', link, 
+             '" target="_blank" class="btn btn-primary">查看全文</a>')
   sprintf(l,val)
 }
 
+we_ui= c("此内容因违规无法查看", "此内容被投诉且经审核涉嫌侵权，无法查看。",
+         "此帐号已被屏蔽,", "该公众号已迁移", "该内容已被发布者删除")
+
 # Define UI for data download app ----
 ui <- function(input, output, session){
+  
   navbarPage(
     title = 'Wechatscope',
-    tabPanel('From 2018-07-06', DT::dataTableOutput('table1'))
-  )
+    tabPanel('Since 2018-07-06'),
+    fluidRow(
+      column(4,
+             selectInput("msg",
+                         "censored_msg",
+                         c("All", we_ui))
+      )
+    ),
+      DT::dataTableOutput("table1")
+    )
 }
 
 
@@ -22,7 +35,14 @@ server <- function(input, output, session) {
   wechat = reactiveFileReader(10000, session, 'ceninfo.csv', read_csv)
   
   output$table1 <- DT::renderDataTable({
+    
     we = wechat()
+    
+    if (input$msg != "All") {
+      we <- we[we$censored_msg == input$msg,]
+    }
+    
+    
     
     we$archive = createLink(we$archive, we$archive)
     
